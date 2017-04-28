eeb7790bd974a89153925a4d31af4ffa0962dbb7
Dany Haddad <danyhaddad43@gmail.com>
2017-04-27 21:53:24 -0700

    improve request styling

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+})
+
+function submit () {
+  var expression = document.getElementById('maxima-expression').value,
+      xhr = new XMLHttpRequest(),
+      displayElem = document.getElementById('result-display');
+  xhr.open("POST", '/maxima', true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+      displayElem.value = xhr.responseText;
+    }
+  }
+  xhr.send('{"expression": "' + expression + '"}');
+}
