7e3adbb7fe55fd54a2a4505b2f8e990c7ca77ffe
Dany Haddad <danyhaddad43@gmail.com>
2017-04-27 23:11:28 -0700

    update path

--- src/maxima.lisp
+++ src/maxima.lisp
@@ -7,37 +7,13 @@
                 :*connection*)
   (:import-from :cl-dbi
                 :connect-cached)
-  (:export ;;:load-maxima
-           :process-input
+  (:export :process-input
            :exo-meval
            :wrap-in-block
            :get-varlist
            :parse-input
            :check-for-termination))
 (in-package :exo-maxima.maxima)
-
-;; (defmethod asdf:perform :around ((o asdf:load-op)
-;;                                  (c asdf:cl-source-file))
-;;   (handler-case (call-next-method o c)
-;;     ;; If a fasl was stale, try to recompile and load (once).
-;;     (sb-ext:invalid-fasl ()
-;;       (asdf:perform (make-instance 'asdf:compile-op) c)
-;;       (call-next-method))))
-
-;; ;;(truename "../quicklisp/dists/maxima/software/maxima-5.36.0.1/src/")
-;; (defpackage make (:lock nil)) ; hack to declare the package but allow it to be
-;;                               ; overwritten when loading maxima-build.lisp
-;; (defun load-maxima (maxima-path)
-;;   (let ((*default-pathname-defaults* maxima-path)
-;;         (*on-package-variance* '(:warn (:swank :swank-backend) :error t)))
-;;     (load "configure.lisp")
-;;     (configure :interactive nil)
-;;     (let ((*default-pathname-defaults* (merge-pathnames #P"src/" maxima-path)))
-;;       (load "maxima-build.lisp")
-;;       (let (;; (make::*compile-during-load* t)
-;;             )
-;;         (maxima-load)))))
-;; (load-maxima (truename "../quicklisp/dists/maxima/software/maxima-5.36.0.1/"))
 
 (defun load-maxima (maxima-path)
   (let ((*default-pathname-defaults* maxima-path))
@@ -48,7 +24,7 @@
       (maxima-load))))
 (defpackage maxima (:lock nil)) ; hack to declare the package but allow it to
                                 ; be overwritten when loading maxima-build.lisp
-(load-maxima (truename "../quicklisp/dists/maxima/software/maxima-master/"))
+(load-maxima (truename "./quicklisp/dists/maxima/software/maxima-master/"))
 
 (defun check-for-termination (input)
   (or (search "$" input :test #'char-equal)
